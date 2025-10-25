91405bb0983de3d7d5e00d23314c239368ec8cd6
asaponhi <asaponhi@gmail.com>
2025-10-25 16:24:29 +0900

    chore: Create Layout folder and add Layout.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
+import Layout from './components/Layout/Layout';
 import Dashboard from './pages/Dashboard';
 import Transactions from './pages/Transactions';
 import Budget from './pages/Budget';
